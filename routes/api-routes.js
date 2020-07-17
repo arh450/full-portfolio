4eba37f79309ba5b7ed91fd07b20342689097dab
hank amadeus <hankamadeus@email.com>
2020-07-17 18:57:57 -0400

    created message model, routed api route for new message request, and on click to post new message

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,20 @@
+const db = require('../models');
 const router = require('express').Router();
 
-router.get('/', (req, res) => res.json('Sample API get endpoint'));
+router.post('/', (req, res) => {
+    db.Message
+        .create({
+            name: req.body.name,
+            email: req.body.email,
+            note: req.body.note
+        })
+        .then((result) => {
+            console.log("Message added successfully");
+            res.json(result);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 module.exports = router;
